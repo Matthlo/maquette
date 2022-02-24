6a6b27f1e487b93fa4e4bdcae19079b4710a702a
Matthlo <matthieu_loncin@live.fr>
2022-02-24 08:42:55 +0100

    WorkingForm

--- src/sections/contact.js
+++ src/sections/contact.js
@@ -1,33 +1,23 @@
-import { useState } from "react";
-import data from "../data/data.json";
+import "./main.css"
+import data from "../data/data.json"
+import "../components/button.css"
 
 function Contact() {
-  const [dataForm, setDataForm] = useState({ name: "", email: "" });
-
-  function handleChange(e) {
-    setDataForm({ ...dataForm, [e.target.name]: e.target.value });
-  }
-
-  function handleSubmit(params) {
-    console.log("submit");
-  }
   return (
-    <form onSubmit={handleSubmit}>
-      <p>
-        <label>
-          {data.titleForMail}
-          <input
-            type="email"
-            name="email"
-            value={dataForm.email}
-            onChange={handleChange}
-          />
-        </label>
-      </p>
-      <p>
-        <button type="submit">{data.buttonForMail}</button>
-      </p>
-    </form>
+    <div>
+    <div className="headerTitle">
+    <p>{data.titleContact}</p>
+    </div>
+    <form name="contact" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact"/>
+          <p>
+            <label><input type="Mail" name="email" placeholder={data.placeholderMail} className="buttonContours" /></label>
+          </p>
+          <p>
+            <button type="submit" className="buttonCircle">{data.buttonForMail}</button>
+          </p>
+        </form>
+        </div>
   );
 }
 
--- src/sections/main.js
+++ src/sections/main.js
@@ -12,18 +12,6 @@ function Main() {
         <h1 className="headerTitle">{data.title}</h1>
         <p className="divMainText">{data.description}</p>
         <button className="button">GET STARTED</button>
-        <form name="contact" method="POST" data-netlify="true">
-          <input type="hidden" name="form-name" value="contact"/>
-          <p>
-          <label>Name <input type="text" name="name" /></label>
-          </p>
-          <p>
-            <label>Email <input type="email" name="email" /></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
       </div>
       
       <div className="divMainRight">
